677a807c763758ea75e7a5f1584943f3124e6759
TaarikaBhatiaPatel <tarickanbhatia@gmail.com>
2022-01-31 21:48:51 -0500

    new commit

--- src/Java/HelloWorld.java
+++ src/Java/HelloWorld.java
@@ -2,6 +2,6 @@ package Java;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("New Commit");
     }
 }
